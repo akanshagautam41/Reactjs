abfb5e9bf58dd6a927d371f975d6c7f76a669bfc
akanshagautam41 <akanshagautam41@gmail.com>
2024-06-24 19:03:21 +0530

    done

--- About.js
+++
@@ -1,9 +0,0 @@
-function About(){
-    return(
-<div className="main">
-<h1>about</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cupiditate ullam veritatis doloribus alias aliquam dolores cum facere, laboriosam minus quasi deleniti dolor facilis.</p>
-</div>
-    );
-}
-export default About;
--- Contact.js
+++
@@ -1,9 +0,0 @@
-function Contact(){
-    return(
-        <div>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores labore dolorem nulla, sed perspiciatis, explicabo quam doloremque commodi quis deserunt iure necessitatibus dolorum ad accusantium?</h1>
-        </div>
-    )
-}
-
-export default Contact;
